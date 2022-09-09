a6fdc40fbe1f7e48bb1cd392ef3026c72f9be0df
Krzysztof Stenografow <flamenco108@gmail.com>
2022-09-09 14:45:12 +0200

    dalej rulesy

--- start-install-plugin-linux.sh
+++ start-install-plugin-linux.sh
@@ -3,4 +3,4 @@
 
 echo "Usuwam słowniki z ~/.config/plover/"
 rm -rf ~/.config/plover/slowik* && echo "Słowniki usunięte" && \
-/home/flamenco/bin/Plover/plover-4.0.0.dev12-x86_64.AppImage -s plover_plugins install /home/flamenco/git/plover_polish_slowik/
+~/bin/Plover/plover-4.0.0.dev12-x86_64.AppImage -s plover_plugins install ~/git/plover_polish_slowik/
